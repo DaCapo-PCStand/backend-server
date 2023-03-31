c3d5435002f1f05c822a9f76d80c6c1a36f972a5
yunyun <cysdl5604@inu.ac.kr>
2023-03-31 15:10:27 +0900

    feat: id 중복 확인 ID 중복 확인 라우터 설정

--- src/routes/member-route.js
+++ src/routes/member-route.js
@@ -4,6 +4,8 @@ const memberController = require('../controllers/member-controller');
 
 // memberRouter로 '/' 경로의 get 요청을 전달 받을 경우 memberController의 findAllMembers 메소드를 실행한다
 router.get('/', memberController.findAllMembers);
+// '/duplicated' 경로의 get 요청 받을 경우 checkDuplicated 메소드로 연결
+router.get('/duplicated', memberController.checkDuplicated);
 // post 요청을 받을 경우 memberController의 registMethod로 연결
 router.post('/', memberController.registMember);
 
