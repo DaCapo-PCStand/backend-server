838ab9782580be2edfa0c206872f93f9b55f859d
yunyun <cysdl5604@inu.ac.kr>
2023-04-03 15:32:04 +0900

    feat: 회원 로그인 구현 비크립트 모듈 이름 수정 및 검증 로직 추가

--- src/auth/password-bcrypt.js
+++ src/auth/password-bcrypt.js
@@ -21,6 +21,7 @@ exports.encrypt = (password) => {
 // 암호화된 패스워드를 받아 일치 여부를 반환하는 메소드 객체
 exports.verify = (inputPassword, encriptedPassword)=>{
     return new Promise((resolve, reject)=>{
+        console.log("[password bcrypt] inputpassword, encriptpassword", [inputPassword, encriptedPassword]);
         bcrypt.compare(inputPassword, encriptedPassword, function(err, result){
              // 비교 중 에러 발생시 reject
             if(err){
@@ -29,7 +30,11 @@ exports.verify = (inputPassword, encriptedPassword)=>{
             }
             
             // 에러가 발생하지 않았다면 결과값을 반환한다
-            resolve(result);
+            if(result) {
+                resolve(true);
+            } else {
+                resolve(false)
+            }
         })
     });
 };
