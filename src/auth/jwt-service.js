7bcb77a0f4bf118981af7c345ad1d1d68fbb0f14
yunyun <cysdl5604@inu.ac.kr>
2023-04-04 01:11:44 +0900

    feat: auth 미들웨어 구현 토큰 강제 만료 로직 제거

--- src/auth/jwt-service.js
+++ src/auth/jwt-service.js
@@ -4,7 +4,7 @@ dotenv.config();
 const secretkey = process.env.SECRET_KEY;
 
 const options = {
-    expiresIn: '24h'
+    expiresIn: '1h'
 }
 
 // 토큰 발급
@@ -16,26 +16,17 @@ exports.sign = (payload) => {
 
 // 토큰 검증
 exports.verify = (token) => {
-    // 추후에 만료 검사를 위해 ignoreExpiration:false 옵션을 부여할 예정
-    // 즉, 현재는 만료 여부를 따지지 않고 복호화해줌
-    return jwt.verify(token, secretkey, (err, decoded)=>{
-        if(err) {
-            console.error(err);
-            return;
-        }
-
-        console.log('toekn 복호화 결과: ' , decoded);
-        return decoded;
-    })
+    return new Promise((resolve, reject)=>{
+        jwt.verify(token, secretkey, {ignoreExpiration:false}, (err, decoded)=>{
+            if(err) {
+                console.error(err);
+                reject(err);
+            }
     
-}
-
-// 토큰 만료
-exports.expire = (token) => {
-    // 토큰 복호화
-    const decoded = jwt.decode(token); 
-    // 현재 시간에서 10초 전으로 만료시간 설정 
-    const newExpireTime =  Math.floor(Date.now() / 1000) -10;
-    // 복호화객체의 만료 시간 변경 => 현재 시간에서 10초 전으로 변경되기때문에 만료됨
-    decoded.exp = newExpireTime;
+    
+            console.log('toekn 복호화 결과: ' , decoded);
+            resolve(decoded);
+        })
+    }) 
+    
 }
