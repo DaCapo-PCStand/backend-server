b49094d9648365402b078c6ca63fbd653ab1a483
yunyun <cysdl5604@inu.ac.kr>
2023-04-06 17:09:56 +0900

    feat: 거치대 등록 해제 API 거치대 등록 해제 라우터 설정

--- src/routes/registration-route.js
+++ src/routes/registration-route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const RegistrationController = require('../controllers/registration-controller');
 
-router.post('/', RegistrationController.registStand);
-
+router.post('/', RegistrationController.registerStand);
+router.delete('/:registrationId', RegistrationController.unregisterStand);
 
 module.exports = router;
